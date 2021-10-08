4f2ae6767da78a34512571a39da58dd44ee70465
avmusic1996 <agvalderrama@misena.edu.co>
2021-10-07 23:26:28 -0500

    Maquetacion login + registro + Dashboard

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>Ecommerce Dashboard &mdash; Stisla</title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('modules/fontawesome/css/all.min.css') }}">
+  <!-- CSS Libraries -->
+
+  <link rel="stylesheet" href="{{ asset('modules/jqvmap/dist/jqvmap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('modules/summernote/summernote-bs4.css') }}">
+  <link rel="stylesheet" href="{{ asset('modules/owlcarousel2/dist/assets/owl.carousel.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('modules/owlcarousel2/dist/assets/owl.theme.default.min.css') }}">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+<!-- Start GA -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+
+
+  {{-- <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>{{ __('Mundo-Factura') }}</title>
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="{{ asset('css/material-dashboard.css?v=2.1.1') }}" rel="stylesheet" /> --}}
+</head>
+
+<body class="{{ $class ?? '' }}">
+  @auth()
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+  </form>
+  @include('layouts.page_templates.auth')
+  @endauth
+
+  @guest()
+    @include('layouts.page_templates.guest')
+  @endguest
+  <!--   Core JS Files   -->
+ 
+  <script src="{{ asset('modules/jquery.min.js') }}"></script>
+  <script src="{{ asset('modules/popper.js') }}"></script>
+  <script src="{{ asset('modules/tooltip.js') }}"></script>
+  <script src="{{ asset('modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('modules/moment.min.js') }}"></script>
+  <script src="{{ asset('modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('js/stisla.js') }}"></script>
+  <script src="{{ asset('modules/jquery.sparkline.min.js') }}"></script>
+  <script src="{{ asset('modules/chart.min.js') }}"></script>
+  <script src="{{ asset('modules/owlcarousel2/dist/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('modules/summernote/summernote-bs4.js') }}"></script>
+  <script src="{{ asset('modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+  <script src="{{ asset('js/page/index.js') }}"></script>
+  <script src="{{ asset('js/scripts.js') }}"></script>
+  <script src="{{ asset('js/custom.js') }}"></script>
+  {{-- <script src="{{ asset('js/plugins/perfect-scrollbar.jquery.min.js') }}"></script> --}}
+  @stack('js')
+</body>
+</html>
---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,265 @@
+<div class="main-sidebar sidebar-style-2">
+  <aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">Stisla</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">St</a>
+    </div>
+    <ul class="sidebar-menu">
+      <li class="menu-header">Dashboard</li>
+      <li class="dropdown active">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+          <li class=active><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+        </ul>
+      </li>
+      <li class="menu-header">Starter</li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+          <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+          <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+        </ul>
+      </li>
+      <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
+          <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
+          <li><a class="nav-link" href="bootstrap-breadcrumb.html">Breadcrumb</a></li>
+          <li><a class="nav-link" href="bootstrap-buttons.html">Buttons</a></li>
+          <li><a class="nav-link" href="bootstrap-card.html">Card</a></li>
+          <li><a class="nav-link" href="bootstrap-carousel.html">Carousel</a></li>
+          <li><a class="nav-link" href="bootstrap-collapse.html">Collapse</a></li>
+          <li><a class="nav-link" href="bootstrap-dropdown.html">Dropdown</a></li>
+          <li><a class="nav-link" href="bootstrap-form.html">Form</a></li>
+          <li><a class="nav-link" href="bootstrap-list-group.html">List Group</a></li>
+          <li><a class="nav-link" href="bootstrap-media-object.html">Media Object</a></li>
+          <li><a class="nav-link" href="bootstrap-modal.html">Modal</a></li>
+          <li><a class="nav-link" href="bootstrap-nav.html">Nav</a></li>
+          <li><a class="nav-link" href="bootstrap-navbar.html">Navbar</a></li>
+          <li><a class="nav-link" href="bootstrap-pagination.html">Pagination</a></li>
+          <li><a class="nav-link" href="bootstrap-popover.html">Popover</a></li>
+          <li><a class="nav-link" href="bootstrap-progress.html">Progress</a></li>
+          <li><a class="nav-link" href="bootstrap-table.html">Table</a></li>
+          <li><a class="nav-link" href="bootstrap-tooltip.html">Tooltip</a></li>
+          <li><a class="nav-link" href="bootstrap-typography.html">Typography</a></li>
+        </ul>
+      </li>
+      <li class="menu-header">Stisla</li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Components</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="components-article.html">Article</a></li>                <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Avatar</a></li>                <li><a class="nav-link" href="components-chat-box.html">Chat Box</a></li>                <li><a class="nav-link beep beep-sidebar" href="components-empty-state.html">Empty State</a></li>                <li><a class="nav-link" href="components-gallery.html">Gallery</a></li>
+          <li><a class="nav-link beep beep-sidebar" href="components-hero.html">Hero</a></li>                <li><a class="nav-link" href="components-multiple-upload.html">Multiple Upload</a></li>
+          <li><a class="nav-link beep beep-sidebar" href="components-pricing.html">Pricing</a></li>                <li><a class="nav-link" href="components-statistic.html">Statistic</a></li>                <li><a class="nav-link" href="components-tab.html">Tab</a></li>
+          <li><a class="nav-link" href="components-table.html">Table</a></li>
+          <li><a class="nav-link" href="components-user.html">User</a></li>                <li><a class="nav-link beep beep-sidebar" href="components-wizard.html">Wizard</a></li>              </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
+          <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
+          <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Google Maps</span></a>
+        <ul class="dropdown-menu">
+          <li><a href="gmaps-advanced-route.html">Advanced Route</a></li>
+          <li><a href="gmaps-draggable-marker.html">Draggable Marker</a></li>
+          <li><a href="gmaps-geocoding.html">Geocoding</a></li>
+          <li><a href="gmaps-geolocation.html">Geolocation</a></li>
+          <li><a href="gmaps-marker.html">Marker</a></li>
+          <li><a href="gmaps-multiple-marker.html">Multiple Marker</a></li>
+          <li><a href="gmaps-route.html">Route</a></li>
+          <li><a href="gmaps-simple.html">Simple</a></li>
+        </ul>
+      </li>            <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i> <span>Modules</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
+          <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
+          <li><a class="nav-link" href="modules-datatables.html">DataTables</a></li>
+          <li><a class="nav-link" href="modules-flag.html">Flag</a></li>
+          <li><a class="nav-link" href="modules-font-awesome.html">Font Awesome</a></li>
+          <li><a class="nav-link" href="modules-ion-icons.html">Ion Icons</a></li>
+          <li><a class="nav-link" href="modules-owl-carousel.html">Owl Carousel</a></li>
+          <li><a class="nav-link" href="modules-sparkline.html">Sparkline</a></li>
+          <li><a class="nav-link" href="modules-sweet-alert.html">Sweet Alert</a></li>
+          <li><a class="nav-link" href="modules-toastr.html">Toastr</a></li>
+          <li><a class="nav-link" href="modules-vector-map.html">Vector Map</a></li>
+          <li><a class="nav-link" href="modules-weather-icon.html">Weather Icon</a></li>
+        </ul>
+      </li>
+      <li class="menu-header">Pages</li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
+        <ul class="dropdown-menu">
+          <li><a href="auth-forgot-password.html">Forgot Password</a></li> 
+          <li><a href="auth-login.html">Login</a></li> 
+          <li><a href="auth-register.html">Register</a></li> 
+          <li><a href="auth-reset-password.html">Reset Password</a></li> 
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i> <span>Errors</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="errors-503.html">503</a></li> 
+          <li><a class="nav-link" href="errors-403.html">403</a></li> 
+          <li><a class="nav-link" href="errors-404.html">404</a></li> 
+          <li><a class="nav-link" href="errors-500.html">500</a></li> 
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i> <span>Features</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="features-activities.html">Activities</a></li>
+          <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
+          <li><a class="nav-link" href="features-posts.html">Posts</a></li>
+          <li><a class="nav-link" href="features-profile.html">Profile</a></li>
+          <li><a class="nav-link" href="features-settings.html">Settings</a></li>
+          <li><a class="nav-link" href="features-setting-detail.html">Setting Detail</a></li>
+          <li><a class="nav-link" href="features-tickets.html">Tickets</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Utilities</span></a>
+        <ul class="dropdown-menu">
+          <li><a href="utilities-contact.html">Contact</a></li>
+          <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
+          <li><a href="utilities-subscribe.html">Subscribe</a></li>
+        </ul>
+      </li>            <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Credits</span></a></li>
+    </ul>
+
+    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+        <i class="fas fa-rocket"></i> Documentation
+      </a>
+    </div>        </aside>
+</div>
+
+{{-- 
+
+
+
+<div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
+  <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+  -->
+  <div class="logo">
+    <a href="https://creative-tim.com/" class="simple-text logo-normal">
+      {{ __('Creative Tim') }}
+    </a>
+  </div>
+  <div class="sidebar-wrapper">
+    <ul class="nav">
+      <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('home') }}">
+          <i class="material-icons">dashboard</i>
+            <p>{{ __('Dashboard') }}</p>
+        </a>
+      </li>
+      <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
+        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
+          <i><img style="width:25px" src="{{ asset('img/laravel.svg') }}"></i>
+          <p>{{ __('Laravel Examples') }}
+            <b class="caret"></b>
+          </p>
+        </a>
+        <div class="collapse show" id="laravelExample">
+          <ul class="nav">
+            <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
+              <a class="nav-link" href="">
+                <span class="sidebar-mini"> UP </span>
+                <span class="sidebar-normal">{{ __('Perfil de usuarios') }} </span>
+              </a>
+            </li>
+            <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
+              <a class="nav-link" href="#">
+                <span class="sidebar-mini"> UM </span>
+                <span class="sidebar-normal"> {{ __('User Management') }} </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      @can('user_index')
+      <li class="nav-item{{ $activePage == 'users' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('users.index') }}">
+          <i class="material-icons">content_paste</i>
+            <p>Usuarios</p>
+        </a>
+      </li>
+      @endcan
+      @can('post_index')
+      <li class="nav-item{{ $activePage == 'posts' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('posts.index') }}">
+          <i class="material-icons">library_books</i>
+            <p>{{ __('Post') }}</p>
+        </a>
+      </li>
+      @endcan
+      @can('post_index')
+      <li class="nav-item{{ $activePage == 'posts' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('posts.index') }}">
+          <i class="material-icons">library_books</i>
+            <p>{{ __('Post') }}</p>
+        </a>
+      </li>
+      @endcan
+      @can('post_index')
+      <li class="nav-item{{ $activePage == 'posts' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('posts.index') }}">
+          <i class="material-icons">library_books</i>
+            <p>{{ __('Post') }}</p>
+        </a>
+      </li>
+      @endcan
+      @can('post_index')
+      <li class="nav-item{{ $activePage == 'permissions' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('permissions.index') }}">
+          <i class="material-icons">library_books</i>
+            <p>{{ __('Post') }}</p>
+        </a>
+      </li>
+      @endcan
+      @can('permission_index')
+      <li class="nav-item{{ $activePage == 'permissions' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('permissions.index') }}">
+          <i class="material-icons">bubble_chart</i>
+          <p>{{ __('Permissions') }}</p>
+        </a>
+      </li>
+      @endcan
+      @can('role_index')
+      <li class="nav-item{{ $activePage == 'roles' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('roles.index') }}">
+          <i class="material-icons">location_ons</i>
+            <p>{{ __('Roles') }}</p>
+        </a>
+      </li>
+      @endcan
+      <li class="nav-item{{ $activePage == 'notifications' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('users.index') }}">
+          <i class="material-icons">notifications</i>
+          <p>{{ __('Notifications') }}</p>
+        </a>
+      </li>
+      <li class="nav-item{{ $activePage == 'language' ? ' active' : '' }}">
+        <a class="nav-link" href="#">
+          <i class="material-icons">language</i>
+          <p>{{ __('RTL Support') }}</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div> --}}
