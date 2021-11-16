3dad25ab622183ee195dc7bbf7232ef49a4db609
Richard Saavedra <richardcmg7@gmail.com>
2021-11-16 17:21:12 -0500

    Ajuste Sidebar y Layouth main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>Ecommerce Dashboard &mdash; Stisla</title>
+  <title>MundoFactura</title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   
 
--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,15 +1,15 @@
 <div class="main-sidebar sidebar-style-2">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="index.html">Stisla</a>
+      <a href="index.html">MundoFactura</a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
       <a href="index.html">St</a>
     </div>
     <ul class="sidebar-menu">
-      <li class="menu-header{{ $activePage == 'dashboard' ? ' active' : '' }}">Dashboard</li>
+      <li class="menu-header{{ $activePage == 'dashboard' ? ' active' : '' }}">Inicio</li>
       <li class="dropdown active">
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>{{ __("Home") }}</span></a>
         <ul class="dropdown-menu">
           <li><a class="nav-link" href="{{ route('home')}}">Reportes General</a></li>
           <li class=active><a class="nav-link" href="#">Reportes especificos</a></li>
@@ -27,129 +27,6 @@
       </li>
       <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Creditos</span></a></li>
     </ul>
-
-    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Adquirir pack premiun
-      </a>
-    </div>        </aside>
+    
+  </aside>
 </div>
-
-{{-- 
-
-
-
-<div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
-  <!--
-      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
-
-      Tip 2: you can also add an image using data-image tag
-  -->
-  <div class="logo">
-    <a href="https://creative-tim.com/" class="simple-text logo-normal">
-      {{ __('Creative Tim') }}
-    </a>
-  </div>
-  <div class="sidebar-wrapper">
-    <ul class="nav">
-      <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('home') }}">
-          <i class="material-icons">dashboard</i>
-            <p>{{ __('Dashboard') }}</p>
-        </a>
-      </li>
-      <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
-        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
-          <i><img style="width:25px" src="{{ asset('img/laravel.svg') }}"></i>
-          <p>{{ __('Laravel Examples') }}
-            <b class="caret"></b>
-          </p>
-        </a>
-        <div class="collapse show" id="laravelExample">
-          <ul class="nav">
-            <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
-              <a class="nav-link" href="">
-                <span class="sidebar-mini"> UP </span>
-                <span class="sidebar-normal">{{ __('Perfil de usuarios') }} </span>
-              </a>
-            </li>
-            <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
-              <a class="nav-link" href="#">
-                <span class="sidebar-mini"> UM </span>
-                <span class="sidebar-normal"> {{ __('User Management') }} </span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </li>
-      @can('user_index')
-      <li class="nav-item{{ $activePage == 'users' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('users.index') }}">
-          <i class="material-icons">content_paste</i>
-            <p>Usuarios</p>
-        </a>
-      </li>
-      @endcan
-      @can('post_index')
-      <li class="nav-item{{ $activePage == 'posts' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('posts.index') }}">
-          <i class="material-icons">library_books</i>
-            <p>{{ __('Post') }}</p>
-        </a>
-      </li>
-      @endcan
-      @can('post_index')
-      <li class="nav-item{{ $activePage == 'posts' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('posts.index') }}">
-          <i class="material-icons">library_books</i>
-            <p>{{ __('Post') }}</p>
-        </a>
-      </li>
-      @endcan
-      @can('post_index')
-      <li class="nav-item{{ $activePage == 'posts' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('posts.index') }}">
-          <i class="material-icons">library_books</i>
-            <p>{{ __('Post') }}</p>
-        </a>
-      </li>
-      @endcan
-      @can('post_index')
-      <li class="nav-item{{ $activePage == 'permissions' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('permissions.index') }}">
-          <i class="material-icons">library_books</i>
-            <p>{{ __('Post') }}</p>
-        </a>
-      </li>
-      @endcan
-      @can('permission_index')
-      <li class="nav-item{{ $activePage == 'permissions' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('permissions.index') }}">
-          <i class="material-icons">bubble_chart</i>
-          <p>{{ __('Permissions') }}</p>
-        </a>
-      </li>
-      @endcan
-      @can('role_index')
-      <li class="nav-item{{ $activePage == 'roles' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('roles.index') }}">
-          <i class="material-icons">location_ons</i>
-            <p>{{ __('Roles') }}</p>
-        </a>
-      </li>
-      @endcan
-      <li class="nav-item{{ $activePage == 'notifications' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('users.index') }}">
-          <i class="material-icons">notifications</i>
-          <p>{{ __('Notifications') }}</p>
-        </a>
-      </li>
-      <li class="nav-item{{ $activePage == 'language' ? ' active' : '' }}">
-        <a class="nav-link" href="#">
-          <i class="material-icons">language</i>
-          <p>{{ __('RTL Support') }}</p>
-        </a>
-      </li>
-    </ul>
-  </div>
-</div> --}}
