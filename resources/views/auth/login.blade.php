ebc720a62e3e3d6b6469fec57469bc511726dc2b
Richard Saavedra <richardcmg7@gmail.com>
2021-11-16 17:20:36 -0500

    Ajuste respuesta formulario de logins

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,14 +36,12 @@
                         </b>
                       </div>
                       <input id="email" type="email" placeholder="Correo electrónico" class="form-control input-login @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" name="email" tabindex="1" required autofocus>
-                          @error('email')
-                              <span class="invalid-feedback" role="alert">
-                                  <strong>{{ $message }}</strong>
-                              </span>
-                          @enderror
-                      <div class="invalid-feedback">
-                        Please fill in your email
-                      </div>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                  
                     </div>
   
                     <div class="form-group">
@@ -55,9 +53,7 @@
                                       <strong>{{ $message }}</strong>
                                   </span>
                                   @enderror
-                      <div class="invalid-feedback">
-                        please fill in your password
-                      </div>
+
                     </div>
 
                     <div class="form-group">
